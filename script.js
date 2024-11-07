105a43bfcea03c1f98a2adc96991241a40b76e82
zephahoo <huzaifahizhar@gmail.com>
2024-11-07 13:48:23 +0800

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 function segregateNames() {
   const inputNames = document.getElementById("inputArea").value;
-  const namesArray = inputNames.split("\n");
+  const namesArray = inputNames.split(/\r?\n/); 
   let almarhumList = [];
   let almarhumahList = [];
 
   for (let name of namesArray) {
-    // Remove hidden characters and normalize spaces
-    let cleanedName = name.trim().replace(/[^\w\s]/gi, '').replace(/\s+/g, ' '); 
-    if (/\b(bin|S\/O)\b/i.test(cleanedName)) { // Added S/O to almarhumList condition
+    let cleanedName = name.trim().replace(/[^\w\s\/]/gi, '').replace(/\s+/g, ' ');
+    cleanedName = cleanedName.toLowerCase(); 
+
+    // Use a single regex with all the patterns
+    if (/\b(bin|s\/o)\b/.test(cleanedName)) {
       almarhumList.push(name);
-    } else if (/\b(binti|D\/O)\b/i.test(cleanedName)) { 
-      almarhumahList.push(name);
-    } 
-    // Additional check for 'binte' (without removing the existing code)
-    else if (/\bbinte\b/i.test(cleanedName)) {
+    } else if (/\b(binti|d\/o|binte)\b/.test(cleanedName)) {
       almarhumahList.push(name);
     }
   }
